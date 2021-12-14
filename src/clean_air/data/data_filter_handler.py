86136a6597c7f6059183feae3219f508d9c3a142
Catherine Lucas <catherine.lucas@metoffice.gov.uk>
2021-12-14 15:43:20 +0000

    Updates to data filter handler following pull request comments

--- src/clean_air/data/data_filter_handler.py
+++ src/clean_air/data/data_filter_handler.py
@@ -81,81 +81,21 @@ class DataFilterHandler:
         self.filters_dict[key] = False
 
 
-    def is_filter_on(self, key)->bool:
-        """Checks if filter is turned on for this dataset.
+    def is_match(self, key)->bool:
+        """Checks if there is a match for this dataset in the filtered list of datasets.
         where the <yaml_filename> is the key"""
 
-        if key in self.filters_dict.keys():
-            if self.filters_dict[key]:
-                return True
-            else:
-                return False
-        else:
-            return False
+        return bool(self.filters_dict.get(key))
 
     def get_filtered_data_subsets(self)->set:
         """ returns a set of keys <yaml_filename> for the filters that are switched on"""
         set_out = set()
 
-        for key, value in self.filters_dict.items():
-            if self.filters_dict[key]:
-                set_out.add(key)
-
-        return set_out
-
-    #TODO: for CHEMICAL species ..if using the turn_filter_on_contains function, will need to rewrite to get to inner lists !!
-
-    # def turn_filter_on_contains(self, filter_key, filter_value):
-    #     """Turn the filter on for any keys that contain the <filter_key> , <filter_value>"""
-    #
-    #     # Loop through yaml_allfiles
-    #     for key, value in self.yaml_allfiles_dict.items():
-    #         # loop through dict contents for each yaml file
-    #         for key1, value1 in value.items():
-    #             if key1 == filter_key and str(value1) == filter_value:
-    #                 print(f'Filter On: Dataset is {key}, key is {key1}, value is {value1}')
-    #                 self.turn_filter_on(key)
-
-    # def turn_filter_off_contains(self, filter_key, filter_value):
-    #     """Turn the filter off for any keys that contain the <filter_key> , <filter_value>"""
-    #
-    #     # Loop through yaml_allfiles
-    #     for key, value in self.yaml_allfiles_dict.items():
-    #         # loop through dict contents for each yaml file
-    #         for key1, value1 in value.items():
-    #             if key1 == filter_key and str(value1) == filter_value:
-    #                 print(f'Filter Off: Dataset is {key}, key is {key1}, value is {value1}')
-    #                 self.turn_filter_off(key)
+        return {key for key, value in self.filters_dict.items() if value}
 
     def get_filters_dict(self):
         return self.filters_dict
 
     def get_allfiles_dict(self):
         return self.yaml_allfiles_dict
-
-
-
-    # TODO: for CHEMICAL SPECIES ..this inner dictionary access not currently being used but is to help me with chemical species filter
-    def print_access_inner_dict(self):
-    #THINK THIS CRAZY BIT OF CODE ACTUALLY DOES WHAT I WANT :-)
-    # PUT THIS IN THE FILTER HANDLER , NOT HERE ..AND HAVE TWO VERSIONS SO CAN UPDATE THE FILTERS ( PLAIN & INNER VERSIONS)
-    #KEEP THIS PRINT VERSION TOO SO CAN SEE WHAT HECK IS GOING ON !!
-        for outer_key in self.get_allfiles_dict():
-            # print('Outer Key = ', outer_key)  #outer key is the yaml filename
-            for inner_key in self.get_allfiles_dict()[outer_key]: #Now we have a possible key/value pair (this enough for simple ones)
-                #print('   ',
-                #      'Inner Key', inner_key,
-                #      'Inner Value', self.get_allfiles_dict()[outer_key][inner_key] ,
-                #      'Inner Value Type', type(self.get_allfiles_dict()[outer_key][inner_key]))
-
-                #Now check to see if inner key is actually a list of key/value pairs
-                if type(self.get_allfiles_dict()[outer_key][inner_key]) is list:
-                   for goal in self.get_allfiles_dict()[outer_key][inner_key]:
-                       #print('      ', type(self.get_allfiles_dict()[outer_key][inner_key]) , goal, type(goal))
-                       #Now get the values in these mini dictionaries
-                       if type(goal) is dict:
-                          for key, value in goal.items():
-                            #print('         ', type(goal), str(key) + " => " + str(value))
-                            pass
-
 
