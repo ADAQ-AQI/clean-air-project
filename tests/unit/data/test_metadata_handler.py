e3cef909a42a7fb97876530f48510c6594ad49cb
Catherine Lucas <catherine.lucas@metoffice.gov.uk>
2021-12-14 12:27:41 +0000

    git tests

--- tests/unit/data/test_metadata_handler.py
+++ tests/unit/data/test_metadata_handler.py
@@ -47,7 +47,7 @@ def antartica_filepath(sampledir):
     filepath = os.path.join(sampledir, "test_data", "antartica_metadata.yaml")
     return filepath
 
-def checkFiltersDictForFilename(dict, filepath):
+def checkDictForFilename(dict, filepath):
     found = False
     for key in dict.keys():
         keysplit = key.rsplit('/', 1)[1]
@@ -59,7 +59,7 @@ def checkFiltersDictForFilename(dict, filepath):
     assert found == True # Filename must be in dictionary
     return found
 
-def getFiltersDictValueOfGivenFilename(dict, filepath):
+def getDictValueOfGivenFilename(dict, filepath):
 
     for key in dict.keys():
         keysplit = key.rsplit('/', 1)[1]
@@ -92,54 +92,44 @@ def checkSetForFilename(this_set, filepath):
 def test_build_filters(station_metadata_filepath, metaone_filepath, antartica_filepath, mh):
     #Make sure all set to true at start.
 
-    checkFiltersDictForFilename(mh.filters_dict, station_metadata_filepath)
-    checkFiltersDictForFilename(mh.filters_dict, metaone_filepath)
-    checkFiltersDictForFilename(mh.filters_dict, antartica_filepath)
+    checkDictForFilename(mh.filters_dict, station_metadata_filepath)
+    checkDictForFilename(mh.filters_dict, metaone_filepath)
+    checkDictForFilename(mh.filters_dict, antartica_filepath)
 
 
 def test_all_filters_true(station_metadata_filepath, metaone_filepath, mh):
-    
-    #assert mh.filters_dict.get(station_metadata_filepath) == True
-    #assert mh.filters_dict.get(metaone_filepath) == True
 
-    assert getFiltersDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) == True
-    assert getFiltersDictValueOfGivenFilename(mh.filters_dict, metaone_filepath) == True
+    assert getDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) == True
+    assert getDictValueOfGivenFilename(mh.filters_dict, metaone_filepath) == True
 
 def test_turn_off_obs_level_ground(station_metadata_filepath, mh):
     #TEST 2: Obs Level:GROUND', ' Turn off all datasets containing Ground')
 
     mh.set_observation_level_filter(False, 'ground')
-    #assert (mh.is_filter_on(station_metadata_filepath))==False
-    assert getFiltersDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) ==False
+    assert getDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) == False
 
 
 def test_turn_on_obs_level_ground(station_metadata_filepath, mh):
     #Test 3 : Obs Level:GROUND', ' Turn on all datasets containing Ground')
 
     mh.set_observation_level_filter(True, 'ground')
-    #assert (mh.is_filter_on(station_metadata_filepath)) ==True
-    assert getFiltersDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) == True
+    assert getDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) == True
 
 
 def test_turn_off_source_air_quality(station_metadata_filepath, mh):
     #Test 4 : Data Source:AIR_QUALITY', ' Turn off all datasets containing Air Quality' )
     mh.set_data_source_filter(False, 'air_quality')
-    #assert(mh.is_filter_on(station_metadata_filepath)) == False
-    assert getFiltersDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) == False
+    assert getDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) == False
 
 def test_turn_on_source_air_quality(station_metadata_filepath, mh):
     #Test 5 : Data Source:AIR_QUALITY', ' Turn on all datasets containing Air Quality')
     mh.set_data_source_filter(True, 'air_quality')
-    #assert(mh.is_filter_on(station_metadata_filepath)) == True
-    assert getFiltersDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) == True
+    assert getDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) == True
 
 def test_filtered_dataset_list_contents(station_metadata_filepath, metaone_filepath, mh):
     #Test 6: Get back a list of filtered datasets
 
-    #assert metaone_filepath in mh.get_filtered_data_subsets()
     checkSetForFilename(mh.get_filtered_data_subsets(), metaone_filepath)
-
-    #assert station_metadata_filepath in mh.get_filtered_data_subsets()
     checkSetForFilename(mh.get_filtered_data_subsets(), station_metadata_filepath)
 
     #print(mh.filters_dict.keys())
