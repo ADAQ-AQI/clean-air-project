163043990e467acaf9cfe85805955e6032a7472e
thomas.wilson <twilson@vld018.cmpd1.metoffice.gov.uk>
2022-05-24 11:06:33 +0100

    restructured generate_map_based_visualisations

--- tests/unit/visualise/test_generate_map_based_visualisations.py
+++ tests/unit/visualise/test_generate_map_based_visualisations.py
@@ -20,10 +20,23 @@ def AURN_filepath(sampledir):
 
 
 @pytest.fixture()
+def AURN_savepath(tmp_output_path):
+    AURN_savepath = os.path.join(tmp_output_path, "AURN.html")
+    return AURN_savepath
+
+
+@pytest.fixture()
 def aircraft_filepath(sampledir):
     aircraft_filepath = os.path.join(sampledir, "aircraft",
                                      "MOCCA_M251_20190903.nc")
     return aircraft_filepath
+
+
+@pytest.fixture()
+def aircraft_savepath(tmp_output_path):
+    aircraft_savepath = os.path.join(tmp_output_path, "AircraftTrack.html")
+    return aircraft_savepath
+
 
 @pytest.fixture()
 def boundaries_filepath(sampledir):
@@ -32,37 +45,30 @@ def boundaries_filepath(sampledir):
     return boundaries_filepath
 
 
-# Tests for get_AURN__sites_site_map:
 @pytest.fixture()
-def AURN_site_map(AURN_filepath, tmp_output_path):
-    save_path = os.path.join(tmp_output_path, "AURN.html")
-    site_map = make_maps.get_aurn_sites_site_map(AURN_filepath, save_path)
-    return site_map
+def boundaries_savepath(tmp_output_path):
+    boundaries_savepath = os.path.join(tmp_output_path, "boundaries.html")
+    return boundaries_savepath
 
 
-def test_AURN_site_map_is_Map(AURN_site_map):
+# Tests for get_AURN__sites_site_map:
+def test_AURN_site_map_is_Map(AURN_filepath, AURN_savepath):
     """Test that AURN site data has been successfully converted to a folium
     Map object."""
-    assert isinstance(AURN_site_map, folium.Map)
+    site_map = make_maps.get_aurn_sites_site_map(AURN_filepath, AURN_savepath)
+    assert isinstance(site_map, folium.Map)
 
 
-def test_AURN_site_map_has_children(AURN_site_map):
+def test_AURN_site_map_has_children(AURN_filepath, AURN_savepath):
     """Test that children (markers) added during map processing are present in
     site_map object."""
     # This is always the same input file, so the number of markers generated
     # should always be the same.
-    assert len(AURN_site_map._children) == 275
+    site_map = make_maps.get_aurn_sites_site_map(AURN_filepath, AURN_savepath)
+    assert len(site_map._children) == 275
 
 
 # Tests for get_aircraft_track_map:
-@pytest.fixture()
-def aircraft_track(aircraft_filepath, tmp_output_path):
-    save_path = os.path.join(tmp_output_path, "AircraftTrack.html")
-    aircraft_track = make_maps.get_aircraft_track_map(aircraft_filepath,
-                                                      save_path)
-    return aircraft_track
-
-
 def test_input_filetype_error(AURN_filepath, tmp_output_path):
     """Test that an error is thrown when a non-netCDF filetype is used as
     input data."""
@@ -71,38 +77,34 @@ def test_input_filetype_error(AURN_filepath, tmp_output_path):
         make_maps.get_aircraft_track_map(AURN_filepath, save_path)
 
 
-def test_aircraft_track_map_is_Map(aircraft_track):
+def test_aircraft_track_map_is_Map(aircraft_filepath, aircraft_savepath):
     """Test that aircraft track data has been successfully converted to a
     folium Map object."""
+    aircraft_track = make_maps.get_aircraft_track_map(aircraft_filepath, aircraft_savepath)
     assert isinstance(aircraft_track, folium.Map)
 
 
-def test_aircraft_track_map_has_children(aircraft_track):
+def test_aircraft_track_map_has_children(aircraft_filepath, aircraft_savepath):
     """Test that children (lines) added during map processing are present in
     site_map object."""
     # Again, this input file is fixed and static so the output should always
     # produce three children.
+    aircraft_track = make_maps.get_aircraft_track_map(aircraft_filepath, aircraft_savepath)
     assert len(aircraft_track._children) == 3
 
 
 # Tests for get_boundaries:
-@pytest.fixture()
-def boundaries(boundaries_filepath, tmp_output_path):
-    save_path = os.path.join(tmp_output_path, "boundaries.html")
-    boundaries = make_maps.get_boundaries(boundaries_filepath,
-                                                      save_path)
-    return boundaries
-
-
-def test_boundaries_map_is_Map(boundaries):
+def test_boundaries_map_is_Map(boundaries_filepath, boundaries_savepath):
     """Test that boundary data has been successfully converted to a folium
     Map object."""
+    boundaries = make_maps.get_boundaries(boundaries_filepath, boundaries_savepath)
     assert isinstance(boundaries, folium.Map)
 
 
-def test_boundaries_map_has_children(boundaries):
+def test_boundaries_map_has_children(boundaries_filepath, boundaries_savepath):
     """Test that children (lines) added during map processing are present in
     site_map object."""
     # This is always the same input file, so the number of children
     # should always be the same.
+    boundaries = make_maps.get_boundaries(boundaries_filepath, boundaries_savepath)
     assert len(boundaries._children) == 2
