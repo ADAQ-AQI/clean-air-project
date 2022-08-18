6fed835153e35304fb0e573f83b7ef6c9458ff57
thomas.wilson <twilson@vld018.cmpd1.metoffice.gov.uk>
2022-08-18 16:56:19 +0100

    began implementation of extracting cubeList metadata

--- tests/unit/data/test_extract_metadata.py
+++ tests/unit/data/test_extract_metadata.py
@@ -1,13 +1,13 @@
 import pytest
 from iris.coords import DimCoord
-from iris.cube import Cube
+from iris.cube import Cube, CubeList
 import numpy as np
 from clean_air import data
 
 class TestExtractMetadata:
 	@staticmethod
 	@pytest.fixture
-	def cube():
+	def cube_1():
 		latitude = DimCoord(np.linspace(-90, 90, 4),
                     standard_name='latitude',
                     units='degrees')
@@ -26,7 +26,38 @@ class TestExtractMetadata:
 		return cube
 
 	@staticmethod
-	def test_extract(cube):
-		a = data.extract_metadata.extract_cube_metadata(cube)
+	@pytest.fixture
+	def cube_2():
+		latitude = DimCoord(np.linspace(0, 100, 100),
+                    standard_name='projection_x_coordinate',
+                    units='meters')
+		longitude = DimCoord(np.linspace(0, 100, 100),
+					standard_name='projection_y_coordinate',
+					units='meters')
+		time = DimCoord(np.linspace(100, 148, 48),
+					standard_name='time',
+					units="hours since 1970-01-01 00:00:00")
+		cube = Cube(np.zeros((100, 100, 48), np.float32),
+					standard_name="mass_fraction_of_carbon_dioxide_in_air",
+					units="l",
+					dim_coords_and_dims=[(latitude, 0),
+										(longitude, 1),
+										(time, 2)])
+		return cube
+
+	@staticmethod
+	@pytest.fixture
+	def cube_list(cube_1, cube_2):
+		return CubeList([cube_1, cube_2])
+
+	@staticmethod
+	def test_extract_cube(cube_1):
+		a = data.extract_metadata.extract_cube_metadata(cube_1)
+		print(a)
+		assert(a.title=="mass_concentration_of_ozone_in_air")
+
+	@staticmethod
+	def test_extract_cubelist(cube_list):
+		a = data.extract_metadata.extract_cubelist_metadata()
 		print(a)
 		assert(a.title=="mass_concentration_of_ozone_in_air")
