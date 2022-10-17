5d201e68dcdb9c38b6c23cf8d19b9b538e29c0f6
Chris Bunney <chris.r.bunney@metoffice.gov.uk>
2022-10-17 16:27:38 +0100

    Update links for GitHub hosted dependencies (edr_server & cap_sample_data) and updated test fixtures for the new packaging of cap_sample_data

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ Configuration for pytest - this file is automatically executed on startup.
 """
 
 from pathlib import Path
-
+import cap_sample_data
 import pytest
 
 
@@ -13,14 +13,12 @@ def pytest_addoption(parser):
     """
     Register custom command-line arguments that can be passed to `pytest`
     """
-    root = Path(__file__).parent
-    default = root.parent / "cap-sample-data"
+    default = Path(cap_sample_data.path)
     parser.addoption(
         "--sampledir",
         default=default,
         type=Path,
-        help="checkout of ADAQ-AQI/cap-sample-data.\n"
-        "Default: `cap-sample-data` as a sibling of this repository."
+        help="cap-sample-data package (https://github.com/ADAQ-AQI/cap-sample-data).\n"
     )
 
 
