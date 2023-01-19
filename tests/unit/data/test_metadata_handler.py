8ec64656a5ac3b366b854117197e18b945eff154
Tom Wilson <thomas.wilson@metoffice.gov.uk>
2023-01-19 14:06:56 +0000

    tidied metadata_handler unit tests

--- tests/unit/data/test_metadata_handler.py
+++ tests/unit/data/test_metadata_handler.py
@@ -1,56 +1,39 @@
 import os
 import pytest
 
-from clean_air.data.metadata_handler import MetadataHandler, dict_printer
+from clean_air.data.metadata_handler import MetadataHandler
 
-#NB: These tests use the test data :
-# station_metadata.yaml
-# metadata_one.yaml
-# antartic_metedata.yaml
 
-# TODO: Tidy all this up!!!
-#('METADATA TEST', 'Build list of all metadata files and turn filters to on by default \n')
 @pytest.fixture()
 def mh(sampledir):
-    mh = MetadataHandler(os.path.join(sampledir, "test_data"))
-    dict_printer(mh.filters_dict)
-    return mh
+    return MetadataHandler(os.path.join(sampledir, "test_data"))
+
 
 @pytest.fixture()
 def station_metadata_filepath(sampledir):
-    #filepath = os.path.join('/', 'net', 'home', 'h05', 'clucas', 'PycharmProjects', 'CleanAirProject', 'tests', 'unit','data', 'station_metadata.yaml')
-    #filepath = '/net/home/h05/clucas/PycharmProjects/CleanAirProject/tests/unit/data/station_metadata.yaml'
-    #return filepath
-    filepath = os.path.join(sampledir, "test_data", "station_metadata.yaml")
-    return filepath
+    return os.path.join(sampledir, "test_data", "station_metadata.yaml")
+
 
 @pytest.fixture()
 def metaone_filepath(sampledir):
-    #filepath = os.path.join('/','net','home','h05','clucas','PycharmProjects','CleanAirProject','tests','unit','data', 'metaone.yaml')
-    #filepath = '/net/home/h05/clucas/PycharmProjects/CleanAirProject/tests/unit/data/metaone.yaml'
-    #return filepath
-    filepath = os.path.join(sampledir, "test_data", "metaone.yaml")
-    return filepath
+    return os.path.join(sampledir, "test_data", "metaone.yaml")
+
 
 @pytest.fixture()
 def antartica_filepath(sampledir):
-    #filepath = os.path.join('/','net','home','h05','clucas','PycharmProjects','CleanAirProject','tests','unit','data', 'metaone.yaml')
-    #filepath = '/net/home/h05/clucas/PycharmProjects/CleanAirProject/tests/unit/data/metaone.yaml'
-    #return filepath
-    filepath = os.path.join(sampledir, "test_data", "antartica_metadata.yaml")
-    return filepath
+    return os.path.join(sampledir, "test_data", "antartica_metadata.yaml")
+
 
 def checkDictForFilename(dict, filepath):
     found = False
     for key in dict.keys():
         keysplit = key.rsplit('/', 1)[1]
-
         filepathsplit = filepath.rsplit('/', 1)[1]
         if keysplit == filepathsplit:
             print("Key:", key, " Keysplit:", keysplit, " Filepathsplit:", filepathsplit)
             found = True
-    assert found == True # Filename must be in dictionary
     return found
+
 
 def getDictValueOfGivenFilename(dict, filepath):
 
@@ -63,6 +46,7 @@ def getDictValueOfGivenFilename(dict, filepath):
             value = dict.get(key)
     return value
 
+
 def checkSetForFilename(this_set, filepath):
     found = False
     for key in this_set:
@@ -72,10 +56,10 @@ def checkSetForFilename(this_set, filepath):
         if keysplit == filepathsplit:
             print("Key:", key, " Keysplit:", keysplit, " Filepathsplit:", filepathsplit)
             found = True
-    assert found == True # Filename must be in dictionary
     return found
 
-def is_filter_on(dict, filepath)->bool:
+
+def is_filter_on(dict, filepath) -> bool:
     ret_type = False
     found = False
     for key in dict.keys():
@@ -89,54 +73,64 @@ def is_filter_on(dict, filepath)->bool:
     return ret_type
 
 
-#TEST 1: 
 def test_build_filters(station_metadata_filepath, metaone_filepath, antartica_filepath, mh):
-    #Make sure all set to true at start.
+    # Make sure all filepaths are in metadata handler dictionary
 
-    checkDictForFilename(mh.filters_dict, station_metadata_filepath)
-    checkDictForFilename(mh.filters_dict, metaone_filepath)
-    checkDictForFilename(mh.filters_dict, antartica_filepath)
+    assert checkDictForFilename(mh.filters_dict, station_metadata_filepath) == True
+    assert checkDictForFilename(mh.filters_dict, metaone_filepath) == True
+    assert checkDictForFilename(mh.filters_dict, antartica_filepath) == True
 
 
 def test_all_filters_true(station_metadata_filepath, metaone_filepath, mh):
+    # Make sure all filters are set to true at start
 
     assert getDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) == True
     assert getDictValueOfGivenFilename(mh.filters_dict, metaone_filepath) == True
 
+
 def test_turn_off_obs_level_ground(station_metadata_filepath, mh):
-    #TEST 2: Obs Level:GROUND', ' Turn off all datasets containing Ground')
+    # Obs Level:GROUND
+    # Turn off all datasets containing Ground
 
     mh.set_observation_level_filter(False, 'ground')
     assert getDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) == False
 
 
 def test_turn_on_obs_level_ground(station_metadata_filepath, mh):
-    #Test 3 : Obs Level:GROUND', ' Turn on all datasets containing Ground')
+    # Obs Level:GROUND
+    # Turn on all datasets containing Ground
 
     mh.set_observation_level_filter(True, 'ground')
     assert getDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) == True
 
 
 def test_turn_off_source_air_quality(station_metadata_filepath, mh):
-    #Test 4 : Data Source:AIR_QUALITY', ' Turn off all datasets containing Air Quality' )
+    # Data Source:AIR_QUALITY
+    # Turn off all datasets containing Air Quality
+
     mh.set_data_source_filter(False, 'air_quality')
     assert getDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) == False
 
+
 def test_turn_on_source_air_quality(station_metadata_filepath, mh):
-    #Test 5 : Data Source:AIR_QUALITY', ' Turn on all datasets containing Air Quality')
+    # Data Source:AIR_QUALITY
+    # Turn on all datasets containing Air Quality
+
     mh.set_data_source_filter(True, 'air_quality')
     assert getDictValueOfGivenFilename(mh.filters_dict, station_metadata_filepath) == True
 
-def test_filtered_dataset_list_contents(station_metadata_filepath, metaone_filepath, mh):
-    #Test 6: Get back a list of filtered datasets
 
-    checkSetForFilename(mh.get_filtered_data_subsets(), metaone_filepath)
-    checkSetForFilename(mh.get_filtered_data_subsets(), station_metadata_filepath)
+def test_filtered_dataset_list_contents(station_metadata_filepath, metaone_filepath, mh):
+    # Get back a list of filtered datasets
+
+    assert checkSetForFilename(mh.get_filtered_data_subsets(), metaone_filepath) == True
+    assert checkSetForFilename(mh.get_filtered_data_subsets(), station_metadata_filepath) == True
+    assert checkSetForFilename(mh.get_filtered_data_subsets(), station_metadata_filepath) == True
+
 
 def test_filter_by_point_location(station_metadata_filepath, metaone_filepath, mh):
-    #Test 7: Filter files by a given point location
+    # Filter files by a given point location
 
     mh.set_location_filter(0.0984, 51.5138)
-
     assert is_filter_on(mh.filters_dict, metaone_filepath) == False
     assert is_filter_on(mh.filters_dict, station_metadata_filepath) == True
